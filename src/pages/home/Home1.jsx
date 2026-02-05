7d6b617d35e8db7f22e97514e30dbaef3f000107
Riya Agrawal <RiyaAgrawal2005@gmail.com>
2026-02-05 11:20:20 +0530

    Update home components and working process

--- src/pages/home/Home1.jsx
+++ src/pages/home/Home1.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import Hero from "../components/myHome/Hero";
-import PremiumPlatform from "../components/myHome/PremiumPlatform";
-import WorkingProcess from "../components/myHome/WorkingProcess";
-import StartCompliance from "../components/myHome/StartCompliance";
-import LegalServices from "../components/myHome/LegalServices";
-import WhoWeAre from "../components/myHome/WhoWeAre";
-import LegalFeatures from "../components/myHome/LegalFeatures";
-import TrustedBrands from "../components/myHome/TrustedBrands";
-import AppDownload from "../components/myHome/AppDownload";
-import Testimonials from "../components/myHome/Testimonials";
-import SuccessStory from "../components/myHome/SuccessStory";
+import Hero from "../../components/myHome/Hero";
+import PremiumPlatform from "../../components/myHome/PremiumPlatform";
+import WorkingProcess from "../../components/myHome/WorkingProcess";
+import StartCompliance from "../../components/myHome/StartCompliance";
+import LegalServices from "../../components/myHome/LegalServices";
+import WhoWeAre from "../../components/myHome/WhoWeAre";
+import LegalFeatures from "../../components/myHome/LegalFeatures";
+import TrustedBrands from "../../components/myHome/TrustedBrands";
+import AppDownload from "../../components/myHome/AppDownload";
+import Testimonials from "../../components/myHome/Testimonials";
+import SuccessStory from "../../components/myHome/SuccessStory";
 
 
 
