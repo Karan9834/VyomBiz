89b4b8d3bc2c8546db594366567b9221c439fc0b
karan7638 <karan2421kumar@gmail.com>
2026-04-27 12:14:28 +0530

    chore: remove unwanted .cjs and .txt files

--- test_regex2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/pages/startup/Global Incorporation/CompanyRegistrationUSA.jsx', 'utf8');
-const match = content.match(/<button([^>]*)>[\s\S]*?(Start Your Registration(?: Now)?)[\s\S]*?<\/button>/g);
-console.log(match);
